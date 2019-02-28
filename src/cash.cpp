95a2226321177487f1df3135b4b67e84c792e6c3
ReaPeRSou1 <bektur.atashev@mail.ru>
2019-02-27 19:25:09 -0800

    undo

--- src/cash.cpp
+++ src/cash.cpp
@@ -5,13 +5,31 @@ using namespace std;
 main ()
 
 {
-    int a;
-    int b;
-    int c;
+    int a, b, c;
 
-    cin a;
-    cin b;
-    cin c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
 
-    
+    if (a>b and a>c)
+    {
+        if (b<c)
+        cout << a-b << endl;
+        if (c<b)
+        cout << a-c << endl;
+    }
+    if (b>a and b>c)
+    {
+        if (a<c)
+        cout << b-a << endl;
+        if (c<a)
+        cout << b-c << endl;
+    }
+    if (c>a and c>b)
+    {
+        if (a<b)
+        cout << c-a << endl;
+        if (b<a)
+        cout << c-b << endl;
+    }
 }
